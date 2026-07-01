e67d023fc2bdf574d9d9f92d58407e2db9e4a0b8
Asim9115 <khanasimali20@gmail.com>
2026-07-01 20:17:43 +0530

    delete sandbox

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -5,12 +5,15 @@ import (
 	"net/http"
 	"os"
 	"github.com/asim9115/containerix/internal/sandbox"
+	"github.com/asim9115/containerix/internal/state"
 	"github.com/asim9115/containerix/router"
 )
 
+
+
 func main() {
 		//intialize sandbox
-	_, err := sandbox.Init(
+	sb, err := sandbox.Init(
 		"containerix",
 		2,
 		"3221225472",
@@ -18,7 +21,8 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	log.Println("Sandbox ready")
+	state.SB.Sandbox = sb
+	log.Println("Sandbox ready ")
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8080"
